16ad4aa57952063a6e15e1ac8639a3a5136a2dcb
Spitzkopf <moshe.github@gmail.com>
2016-04-23 18:32:27 +0300

    added optional_storage, added base for generator_iterator

--- mmgen/generator.h
+++ mmgen/generator.h
@@ -3,6 +3,7 @@
 #include <utility>
 #include <type_traits>
 #include <functional>
+#include <iterator>
 
 
 namespace mmgen
@@ -29,6 +30,102 @@ struct type_helper
 	using const_pointer_type = typename const T*;
 	using rvalue_reference_type = typename T&&;
 };
+
+template<typename T>
+struct optional_storage : public type_helper<T>
+{
+	optional_storage()
+	{
+	}
+
+	optional_storage(const_reference_type value)
+	{
+		new (&m_storage) value_type(value);
+		m_valid = true;
+	}
+
+	optional_storage(rvalue_reference_type value)
+	{
+		new (&m_storage) value_type(std::forward<value_type>(value));
+		m_valid = true;
+	}
+
+	optional_storage(const optional_storage& rhs)
+	{
+		new (&m_storage) value_type(*rhs);
+		m_valid = true;
+	}
+
+	optional_storage& operator=(const optional_storage& rhs)
+	{
+		if (this != &rhs) {
+			destroy();
+			new (&m_storage) value_type(*rhs);
+			m_valid = true;
+		}
+		return *this;
+	}
+
+	optional_storage(optional_storage&& rhs)
+		: m_storage{ std::move(rhs.m_storage) }
+		, m_valid{ rhs.m_valid }
+	{
+		rhs.m_valid = false;
+	}
+
+	optional_storage& operator=(optional_storage&& rhs)
+	{
+		if (this != &rhs) {
+			destroy();
+			m_storage = std::move(rhs.m_storage);
+			m_valid = rhs.m_valid;
+			rhs.m_valid = false;
+		}
+		return *this;
+	}
+
+	optional_storage& operator=(rvalue_reference_type value)
+	{
+		*this = optional_storage(std::forward<value_type>(value));
+		return *this;
+	}
+
+	~optional_storage()
+	{
+		destroy();
+	}
+
+	reference_type operator*()
+	{
+		return *get_ptr();
+	}
+
+	const_reference_type operator*() const
+	{
+		return *get_ptr();
+	}
+
+	pointer_type get_ptr()
+	{
+		return reinterpret_cast<pointer_type>(&m_storage);
+	}
+
+	const_pointer_type get_ptr() const
+	{
+		return reinterpret_cast<const_pointer_type>(&m_storage);
+	}
+
+	void destroy()
+	{
+		if (m_valid) {
+			get_ptr()->~T();
+			m_valid = false;
+		}
+	}
+
+	bool m_valid;
+	detail::type_storage<value_type> m_storage;
+};
 }
 
 struct generation_ended_exception : public std::exception
@@ -44,42 +141,55 @@ public:
 	yield_result() { throw generation_ended_exception(); }
 
 	yield_result(const_reference_type value)
+		: m_storage(value)
 	{
-		new (&m_storage) value_type(value);
 	}
 
 	yield_result(rvalue_reference_type value)
-	{
-		new (&m_storage) value_type(std::forward<value_type>(value));
-	}
+		: m_storage(std::forward<value_type>(value))
+	{}
 
-	~yield_result()
-	{
-		get_ptr()->~T();
-	}
+	~yield_result() = default;
 
 	reference_type operator*()
 	{
-		return *get_ptr();
+		return *m_storage;
 	}
 
 	const_reference_type operator*() const
 	{
-		return *get_ptr();
+		return *m_storage;
 	}
 
 private:
-	pointer_type get_ptr()
+	detail::optional_storage<value_type> m_storage;
+};
+
+//forward declaration
+template<typename T>
+class generator;
+
+template<typename T>
+class generator_iterator : 
+	public std::iterator<std::input_iterator_tag, T>
+{
+	friend class generator<T>;
+private:
+	generator_iterator(detail::generator_function<T>* generator)
+		: m_generator{ generator }
+		, m_exhausted{ false }
 	{
-		return reinterpret_cast<pointer_type>(&m_storage);
+		try {
+			m_value = **m_generator();
+		} catch (const generation_ended_exception&) {
+			m_generator = nullptr;
+			m_exhausted = true;
+		}
 	}
 
-	const_pointer_type get_ptr() const
-	{
-		return reinterpret_cast<const_pointer_type>(&m_storage);
-	}
-
-	detail::type_storage<value_type> m_storage;
+	detail::optional_storage<T> m_value;
+	bool m_exhausted;
+	detail::generator_function<T>* m_generator;
 };
 
 template<typename T>
